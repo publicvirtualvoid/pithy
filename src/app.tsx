16ca735f14db1cb2600773da9507b8c6ae09f917
Scott de Jong <scott.dejong@stretchsense.com>
2022-11-13 17:00:36 +0000

    add grid system with sass/bootstrap/theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,33 @@
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from './db/db';
+import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+
+const columns = [
+  { name: 'id', title: 'ID' },
+  { name: 'product', title: 'Product' },
+  { name: 'owner', title: 'Owner' },
+];
+const rows = [
+  { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
+  { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
+];
 
 function App() {
   const transactions = useLiveQuery(() => db.transactions.toArray());
   return (
     <div>
       <h1>Hello world!</h1>
+
       {transactions?.map((t) => <li key={t.id}>{t.date.toString()}</li>)}
+      <div className="card">
+        <Grid
+          rows={rows}
+          columns={columns}
+        >
+          <Table />
+          <TableHeaderRow />
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import './style.css';
+import './style.scss';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
