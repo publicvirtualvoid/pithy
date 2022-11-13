1324e6ef470f12bdda7e98c7cff6548acc7c9df8
Scott de Jong <scott.dejong@stretchsense.com>
2022-11-13 18:12:39 +0000

    remove sass, too slow

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import './style.scss';
-import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
+import './style.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
