7df8e3d6008c422b9947f6b8c5d0024db5bf8c3f
Scott de Jong <scott.dejong@stretchsense.com>
2022-11-14 20:35:37 +0000

    dynamically show tables

--- src/app.tsx
+++ src/app.tsx
@@ -2,28 +2,33 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from './db/db';
 import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
 import { Column } from '@devexpress/dx-react-grid';
+import { Tabs, Tab } from 'react-bootstrap';
 
 const columns: Column[] = [
   { name: 'id', title: 'ID' },
   { name: 'name', title: 'Name' },
-  { name: 'code', title: 'Code' },
+  { name: 'code', title: 'ISO Code' },
   { name: 'symbol', title: 'Symbol' },
 ];
 
 function App() {
-  const transactions = useLiveQuery(() => db.assetTypes.toArray()) ?? [];
   return (
-    <div>
-      <div className="card">
-        <Grid
-          rows={transactions}
-          columns={columns}
-        >
-          <Table />
-          <TableHeaderRow />
-        </Grid>
-      </div>
-    </div>
+    <>
+      <Tabs>
+        {(db as any)._storeNames.map((key: string) => {
+          const rows = useLiveQuery(() => ((db as any)[key] as any).toArray()) ?? [];
+          return (<Tab key={key} eventKey={key} title={key}>
+            <Grid
+              rows={rows}
+              columns={columns}
+            >
+              <Table />
+              <TableHeaderRow />
+            </Grid>
+          </Tab>);
+        })}
+      </Tabs>
+    </>
   );
 }
 
