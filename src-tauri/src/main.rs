b14cffa8c0d37f10f6aa50d54515d32910d987eb
Scott de Jong <scott.dejong@stretchsense.com>
2022-11-14 16:57:29 +0000

    attempt at conditionally loaded react-devtools
    TAURI_DEBUG is only set for your frontend
    (at build/bundle time)
    scottmail — Today at 4:49 PM
    Is there any equivalent for the Rust side? I need to do some conditional stuff before the FE loads
    FabianLars — Today at 4:51 PM
    what's the condition? Like just debug vs prod? or dev vs build (there's a difference actually)
    
    Anyway, you probably can use rust's cfg flags
    scottmail — Today at 4:52 PM
    prefer debug vs prod but happy with dev vs build
    FabianLars — Today at 4:52 PM
    return if cfg!(dev) {"development"} else {"none"}
    and for debug vs prod you can use cfg!(debug_assertions) i think

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,21 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::env;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+#[cfg(debug_assertions)]
+fn env() -> String {
+    return match env::var("ENV") {
+        Ok(val) => val,
+        Err(_e) => "none".to_string(),
+    };
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![env])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
