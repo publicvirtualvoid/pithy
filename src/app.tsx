d80ac9ddfb5b97ba7a8ea9ca307f455f9578ab33
Scott de Jong <scott.dejong@stretchsense.com>
2022-11-14 23:04:46 +0000

    add dummy row detail

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,33 @@
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from './db/db';
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+import { Grid, Table, TableHeaderRow, TableRowDetail } from '@devexpress/dx-react-grid-bootstrap4';
 import { Tabs, Tab } from 'react-bootstrap';
+import { useState } from 'react';
+import { RowDetailState } from '@devexpress/dx-react-grid';
+import { Table as DbTable } from 'dexie';
 
 function App() {
+  const [expandedRowIds, setExpandedRowIds] = useState([2, 5] as (string | number)[]);
   return (
     <>
       <Tabs>
         {(db as any)._storeNames.map((key: string) => {
-          const rows = useLiveQuery(() => ((db as any)[key] as any).toArray()) ?? [];
+          const rows = useLiveQuery(() => ((db as any)[key] as DbTable<any, number>).toArray()) as any[];
+          const columns = rows.length > 0 ? Object.keys(rows[0]).map(k => ({ name: k, title: k })) : [{ name: 'no data', title: 'no data' }];
           return (<Tab key={key} eventKey={key} title={key}>
             <Grid
               rows={rows}
-              columns={rows.length > 0 ? Object.keys(rows[0]).map(k => ({ name: k, title: k })) : [{ name: 'no data', title: 'no data' }]}
+              columns={columns}
             >
+              <RowDetailState
+                expandedRowIds={expandedRowIds}
+                onExpandedRowIdsChange={setExpandedRowIds}
+              />
               <Table />
               <TableHeaderRow />
+              <TableRowDetail
+                contentComponent={() => <div>foo!</div>}
+              />
             </Grid>
           </Tab>);
         })}
